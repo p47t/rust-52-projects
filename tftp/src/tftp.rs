dfb2f37ef020602e4867a470a46bf5e8145c242c
Patrick Tsai <patrick@simplypatrick.com>
2019-04-05 16:53:26 -0700

    Can convert between bytes and Packet

--- tftp/src/tftp.rs
+++ tftp/src/tftp.rs
@@ -1,6 +1,7 @@
 use bytes::{Buf, BufMut, BytesMut};
-use std::io::{Cursor, Read};
+use std::io::{Cursor, Read, BufRead};
 
+#[derive(PartialEq, Debug)]
 pub enum Packet {
     ReadRequest {
         filename: String,
@@ -30,49 +31,89 @@ const OP_ACK: u16 = 4;
 const OP_ERROR: u16 = 5;
 
 impl Packet {
-    pub fn parse(payload: &[u8]) -> Option<Packet> {
-        let mut buf = Cursor::new(payload);
-        match buf.get_u16_be() {
+    pub fn from(payload: &[u8]) -> Option<Packet> {
+        let mut cursor = Cursor::new(payload);
+        match cursor.get_u16_be() {
             OP_RRQ => Some(Packet::ReadRequest {
-                filename: "".to_owned(),
-                mode: "".to_owned(),
+                filename: Self::read_cstr(&mut cursor),
+                mode: Self::read_cstr(&mut cursor),
             }),
             OP_WRQ => Some(Packet::WriteRequest {
-                filename: "".to_owned(),
-                mode: "".to_owned(),
+                filename: Self::read_cstr(&mut cursor),
+                mode: Self::read_cstr(&mut cursor),
             }),
             OP_DATA => Some(Packet::Data {
-                block_num: 0,
-                data: Vec::new(),
+                block_num: cursor.get_u16_be(),
+                data: {
+                    let mut vec = Vec::<u8>::new();
+                    cursor.read_to_end(&mut vec);
+                    vec
+                },
             }),
             OP_ACK => Some(Packet::Ack {
-                block_num: 0,
+                block_num: cursor.get_u16_be(),
             }),
             OP_ERROR => Some(Packet::Error {
-                error_code: 0,
-                error_msg: "".to_owned(),
+                error_code: cursor.get_u16_be(),
+                error_msg: Self::read_cstr(&mut cursor),
             }),
             _ => None
         }
     }
 
-    pub fn generate(packet: &Packet) -> Vec<u8> {
+    fn read_cstr(cursor: &mut Cursor<&[u8]>) -> String {
+        let mut cstr = String::new();
+        loop {
+            let b = cursor.get_u8();
+            if b == 0u8 {
+                break;
+            }
+            cstr.push(b as char);
+        }
+        cstr
+    }
+
+    fn read_null_terminated_utf8(cursor: &mut Cursor<&[u8]>) -> String {
+        let mut vec = Vec::<u8>::new();
+        cursor.read_until(0u8, &mut vec);
+        String::from_utf8(vec).unwrap()
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
         let mut buf = BytesMut::with_capacity(1024);
-        match packet {
-            Packet::ReadRequest { .. } => {
+        match self {
+            Packet::ReadRequest { filename, mode } => {
                 buf.put_u16_be(OP_RRQ);
+
+                filename.bytes().for_each(|b| buf.put(b));
+                buf.put(0u8);
+
+                mode.bytes().for_each(|b| buf.put(b));
+                buf.put(0u8);
             }
-            Packet::WriteRequest { .. } => {
+            Packet::WriteRequest { filename, mode } => {
                 buf.put_u16_be(OP_WRQ);
+
+                filename.bytes().for_each(|b| buf.put(b));
+                buf.put(0u8);
+
+                mode.bytes().for_each(|b| buf.put(b));
+                buf.put(0u8);
             }
-            Packet::Data { .. } => {
+            Packet::Data { block_num, data } => {
                 buf.put_u16_be(OP_DATA);
+                buf.put_u16_be(*block_num);
+                buf.put_slice(data);
             }
-            Packet::Ack { .. } => {
+            Packet::Ack { block_num } => {
                 buf.put_u16_be(OP_ACK);
+                buf.put_u16_be(*block_num);
             }
-            Packet::Error { .. } => {
+            Packet::Error { error_code, error_msg } => {
                 buf.put_u16_be(OP_ERROR);
+                buf.put_u16_be(*error_code);
+                error_msg.bytes().for_each(|b| buf.put(b));
+                buf.put(0u8);
             }
         }
         buf.to_vec()
@@ -100,5 +141,117 @@ impl Sender {
 
     pub fn process(&self, _packet: &Packet) -> Option<Packet> {
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tftp::Packet;
+
+    #[test]
+    fn test_packet_parse() {
+        let bytes = [
+            00u8, 0x01,
+            0x72, 0x66, 0x63, 0x31, 0x33, 0x35, 0x30, 0x2e, 0x74, 0x78, 0x74, 0x00,
+            0x6f, 0x63, 0x74, 0x65, 0x74, 0x00
+        ];
+        assert_eq!(Packet::from(&bytes),
+                   Some(Packet::ReadRequest {
+                       filename: "rfc1350.txt".to_owned(),
+                       mode: "octet".to_owned(),
+                   }));
+
+        let bytes = [
+            00u8, 0x02,
+            0x72, 0x66, 0x63, 0x31, 0x33, 0x35, 0x30, 0x2e, 0x74, 0x78, 0x74, 0x00,
+            0x6f, 0x63, 0x74, 0x65, 0x74, 0x00
+        ];
+        assert_eq!(Packet::from(&bytes),
+                   Some(Packet::WriteRequest {
+                       filename: "rfc1350.txt".to_owned(),
+                       mode: "octet".to_owned(),
+                   }));
+
+        let bytes = [
+            00u8, 0x03,
+            0x00, 0x01,
+            0x0a, 0x0a,
+        ];
+        assert_eq!(Packet::from(&bytes),
+                   Some(Packet::Data {
+                       block_num: 1,
+                       data: vec![0x0a, 0x0a],
+                   }));
+
+        let bytes = [
+            00u8, 0x04,
+            0x00, 0x01,
+        ];
+        assert_eq!(Packet::from(&bytes),
+                   Some(Packet::Ack {
+                       block_num: 1
+                   }));
+
+        let bytes = [
+            00u8, 0x05,
+            0x00, 0x02,
+            0x64, 0x65, 0x6e, 0x69, 0x65, 0x64, 0x00,
+        ];
+        assert_eq!(Packet::from(&bytes),
+                   Some(Packet::Error {
+                       error_code: 2,
+                       error_msg: "denied".to_owned(),
+                   }));
+    }
+
+    #[test]
+    fn test_packet_serialization() {
+        let bytes = [
+            00u8, 0x01,
+            0x72, 0x66, 0x63, 0x31, 0x33, 0x35, 0x30, 0x2e, 0x74, 0x78, 0x74, 0x00,
+            0x6f, 0x63, 0x74, 0x65, 0x74, 0x00
+        ];
+        assert_eq!(Packet::ReadRequest {
+                       filename: "rfc1350.txt".to_owned(),
+                       mode: "octet".to_owned(),
+                   }.to_bytes(), bytes);
+
+        let bytes = [
+            00u8, 0x02,
+            0x72, 0x66, 0x63, 0x31, 0x33, 0x35, 0x30, 0x2e, 0x74, 0x78, 0x74, 0x00,
+            0x6f, 0x63, 0x74, 0x65, 0x74, 0x00
+        ];
+        assert_eq!(Packet::WriteRequest {
+                       filename: "rfc1350.txt".to_owned(),
+                       mode: "octet".to_owned(),
+                   }.to_bytes(), bytes);
+
+        let bytes = [
+            00u8, 0x03,
+            0x00, 0x01,
+            0x0a, 0x0a,
+        ];
+        assert_eq!(Packet::Data {
+                       block_num: 1,
+                       data: vec![0x0a, 0x0a],
+                   }.to_bytes(), bytes);
+
+        let bytes = [
+            00u8, 0x04,
+            0x00, 0x01,
+        ];
+        assert_eq!(Packet::Ack {
+                       block_num: 1
+                   }.to_bytes(), bytes);
+
+        let bytes = [
+            00u8, 0x05,
+            0x00, 0x02,
+            0x64, 0x65, 0x6e, 0x69, 0x65, 0x64, 0x00,
+        ];
+        assert_eq!(Packet::Error {
+                       error_code: 2,
+                       error_msg: "denied".to_owned(),
+                   }.to_bytes(), bytes);
     }
 }
