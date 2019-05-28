af7787dde3abbf5d9f9797e1eabd63699b351074
Patrick Tsai <patrick@simplypatrick.com>
2019-05-28 12:08:19 -0700

    [clog] Refactoring style sheet

--- clog/src/fuchsia.rs
+++ clog/src/fuchsia.rs
@@ -27,71 +27,71 @@ lazy_static! {
     ).unwrap();
 }
 
-fn to_tag(t: &str) -> Option<&'static str> {
+fn tag_to_class(t: &str) -> Option<&'static str> {
     match t {
-        "ERROR" => Some("error"),
-        "WARNING" => Some("warning"),
-        "INFO" => Some("info"),
+        "ERROR" => Some(".error"),
+        "WARNING" => Some(".warning"),
+        "INFO" => Some(".info"),
         _ => None,
     }
 }
 
 pub fn parse_line(line: &str) -> Result<Vec<Field>, std::option::NoneError> {
     if let Some(cap) = RE_LOG.captures(line) {
-        let tag = to_tag(cap.name("tag")?.as_str())?;
+        let class = tag_to_class(cap.name("tag")?.as_str())?;
         Ok(vec![
-            Field::new("[", "time", cap.name("time0")?.as_str(), "]"),
-            Field::new(" ", "time", cap.name("time1")?.as_str(), ">"),
-            Field::new(" [", tag, cap.name("tag")?.as_str(), ":"),
-            Field::pos("source", cap.name("source")?.as_str(), "]"),
-            Field::pre(" ", tag, cap.name("text")?.as_str()),
+            Field::new("[", ".time", cap.name("time0")?.as_str(), "]"),
+            Field::new(" ", ".time", cap.name("time1")?.as_str(), ">"),
+            Field::new(" [", class, cap.name("tag")?.as_str(), ":"),
+            Field::pos(".source", cap.name("source")?.as_str(), "]"),
+            Field::pre(" ", class, cap.name("text")?.as_str()),
         ])
     } else if let Some(cap) = RE_KERNEL_LOG.captures(line) {
         let mut ret = vec![
-            Field::new("[", "time", cap.name("time0")?.as_str(), "]"),
-            Field::new(" ", "time", cap.name("time1")?.as_str(), ">"),
+            Field::new("[", ".time", cap.name("time0")?.as_str(), "]"),
+            Field::new(" ", ".time", cap.name("time1")?.as_str(), ">"),
         ];
         if let Some(_) = cap.name("content") {
             if let Some(_) = cap.name("tag") {
-                let raw_tag = cap.name("tag")?.as_str();
-                if let Some(tag) = to_tag(raw_tag) {
+                let tag = cap.name("tag")?.as_str();
+                if let Some(class) = tag_to_class(tag) {
                     ret.extend(vec![
-                        Field::new(" ", tag, raw_tag, ":"),
+                        Field::new(" ", class, tag, ":"),
                     ]);
                 } else {
                     // treat it as source
                     ret.extend(vec![
-                        Field::new(" ", "source", raw_tag, ":"),
+                        Field::new(" ", ".source", tag, ":"),
                     ]);
                 }
                 if let Some(_) = cap.name("source") {
                     ret.extend(vec![
-                        Field::new(" ", "source", cap.name("source")?.as_str(), ":"),
+                        Field::new(" ", ".source", cap.name("source")?.as_str(), ":"),
                     ]);
                 }
-                if let Some(tag) = to_tag(raw_tag) {
+                if let Some(class) = tag_to_class(tag) {
                     ret.extend(vec![
-                        Field::pre(" ", tag, cap.name("text")?.as_str()),
+                        Field::pre(" ", class, cap.name("text")?.as_str()),
                     ]);
                 } else {
                     ret.extend(vec![
-                        Field::pre(" ", "text", cap.name("text")?.as_str()),
+                        Field::pre(" ", ".text", cap.name("text")?.as_str()),
                     ]);
                 }
             } else if let Some(_) = cap.name("source") {
                 ret.extend(vec![
-                    Field::new(" ", "source", cap.name("source")?.as_str(), ":"),
-                    Field::pre(" ", "text", cap.name("text")?.as_str()),
+                    Field::new(" ", ".source", cap.name("source")?.as_str(), ":"),
+                    Field::pre(" ", ".text", cap.name("text")?.as_str()),
                 ]);
             } else {
                 ret.extend(vec![
-                    Field::pre(" ", "text", cap.name("text")?.as_str()),
+                    Field::pre(" ", ".text", cap.name("text")?.as_str()),
                 ]);
             }
         }
         Ok(ret)
     } else {
-        Ok(vec![Field::new("", "text", line.trim(), "")])
+        Ok(vec![Field::new("", ".text", line.trim(), "")])
     }
 }
 
@@ -103,43 +103,43 @@ mod tests {
     fn test_log() {
         let r = parse_line("[00050.844] 14025:14037>").unwrap();
         assert_eq!(r, vec![
-            Field::new("[", "time", "00050.844", "]"),
-            Field::new(" ", "time", "14025:14037", ">"),
-            Field::pre(" ", "text", ""),
+            Field::new("[", ".time", "00050.844", "]"),
+            Field::new(" ", ".time", "14025:14037", ">"),
+            Field::pre(" ", ".text", ""),
         ]);
 
         let r = parse_line("[00050.844] 14025:14037> INIT: cpu 0, calling hook").unwrap();
         assert_eq!(r, vec![
-            Field::new("[", "time", "00050.844", "]"),
-            Field::new(" ", "time", "14025:14037", ">"),
-            Field::new(" ", "source", "INIT", ":"),
-            Field::pre(" ", "text", "cpu 0, calling hook"),
+            Field::new("[", ".time", "00050.844", "]"),
+            Field::new(" ", ".time", "14025:14037", ">"),
+            Field::new(" ", ".source", "INIT", ":"),
+            Field::pre(" ", ".text", "cpu 0, calling hook"),
         ]);
 
         let r = parse_line("[00050.844] 14025:14037> WARNING: unable to find any cache levels.").unwrap();
         assert_eq!(r, vec![
-            Field::new("[", "time", "00050.844", "]"),
-            Field::new(" ", "time", "14025:14037", ">"),
-            Field::new(" ", "warning", "WARNING", ":"),
-            Field::pre(" ", "warning", "unable to find any cache levels."),
+            Field::new("[", ".time", "00050.844", "]"),
+            Field::new(" ", ".time", "14025:14037", ">"),
+            Field::new(" ", ".warning", "WARNING", ":"),
+            Field::pre(" ", ".warning", "unable to find any cache levels."),
         ]);
 
         let r = parse_line("[00050.844] 14025:14037> ERROR: setupLoaderTermPhysDevs: Failed to detect any valid GPUs in the current config").unwrap();
         assert_eq!(r, vec![
-            Field::new("[", "time", "00050.844", "]"),
-            Field::new(" ", "time", "14025:14037", ">"),
-            Field::new(" ", "error", "ERROR", ":"),
-            Field::new(" ", "source", "setupLoaderTermPhysDevs", ":"),
-            Field::pre(" ", "error", "Failed to detect any valid GPUs in the current config"),
+            Field::new("[", ".time", "00050.844", "]"),
+            Field::new(" ", ".time", "14025:14037", ">"),
+            Field::new(" ", ".error", "ERROR", ":"),
+            Field::new(" ", ".source", "setupLoaderTermPhysDevs", ":"),
+            Field::pre(" ", ".error", "Failed to detect any valid GPUs in the current config"),
         ]);
 
         let r = parse_line("[00050.844] 14025:14037> [INFO:namespace_builder.cc(44)] config-data for fonts").unwrap();
         assert_eq!(r, vec![
-            Field::new("[", "time", "00050.844", "]"),
-            Field::new(" ", "time", "14025:14037", ">"),
-            Field::new(" [", "info", "INFO", ":"),
-            Field::pos("source", "namespace_builder.cc(44)", "]"),
-            Field::pre(" ", "info", "config-data for fonts"),
+            Field::new("[", ".time", "00050.844", "]"),
+            Field::new(" ", ".time", "14025:14037", ">"),
+            Field::new(" [", ".info", "INFO", ":"),
+            Field::pos(".source", "namespace_builder.cc(44)", "]"),
+            Field::pre(" ", ".info", "config-data for fonts"),
         ]);
     }
 }
--- clog/src/main.rs
+++ clog/src/main.rs
@@ -7,45 +7,59 @@ use colored::*;
 
 mod fuchsia;
 
+struct StyleSheet<'a> {
+    inner: HashMap<&'a str, &'a str>,
+}
+
+impl<'a> StyleSheet<'a> {
+    fn new(init: Vec<(&'a str, &'a str)>) -> Self {
+        StyleSheet { inner: init.iter().cloned().collect() }
+    }
+
+    fn get(&self, class: &str) -> Result<&str, NoneError> {
+        Ok(*self.inner.get(class)?)
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct Field<'a> {
     prefix: &'static str,
-    tag: &'static str,
+    class: &'static str,
     content: &'a str,
-    postfix: &'static str
+    postfix: &'static str,
 }
 
 impl<'a> Field<'a> {
-    fn new(prefix: &'static str, tag: &'static str, content: &'a str, postfix: &'static str) -> Self {
-        Field{prefix, tag, content, postfix}
+    fn new(prefix: &'static str, class: &'static str, content: &'a str, postfix: &'static str) -> Self {
+        Field { prefix, class, content, postfix }
     }
 
-    fn pos(tag: &'static str, content: &'a str, postfix: &'static str) -> Self {
-        Field{prefix: "", tag, content, postfix}
+    fn pos(class: &'static str, content: &'a str, postfix: &'static str) -> Self {
+        Field { prefix: "", class, content, postfix }
     }
 
-    fn pre(prefix: &'static str, tag: &'static str, content: &'a str) -> Self {
-        Field{prefix, tag, content, postfix: ""}
+    fn pre(prefix: &'static str, class: &'static str, content: &'a str) -> Self {
+        Field { prefix, class, content, postfix: "" }
     }
 
-    fn format(&self, style: &HashMap<&str, &str>) -> Result<String, NoneError> {
+    fn format(&self, style_sheet: &StyleSheet) -> Result<String, NoneError> {
         Ok(format!("{}{}{}",
-                   self.prefix.color(*style.get("text")?),
-                   self.content.color(*style.get(self.tag)?),
-                   self.postfix.color(*style.get("text")?)))
+                   self.prefix.color(style_sheet.get(".text")?),
+                   self.content.color(style_sheet.get(self.class)?),
+                   self.postfix.color(style_sheet.get(".text")?)))
     }
 }
 
 fn main() -> Result<(), NoneError> {
-    let style: HashMap<&str, &str> = [
-        ("text", "white"),
-        ("time", "cyan"),
-        ("source", "green"),
-        ("thread", "cyan"),
-        ("info", "white"),
-        ("warning", "magenta"),
-        ("error", "red"),
-    ].iter().cloned().collect();
+    let style_sheet = StyleSheet::new(vec![
+        (".text", "white"),
+        (".time", "cyan"),
+        (".source", "green"),
+        (".thread", "cyan"),
+        (".info", "white"),
+        (".warning", "magenta"),
+        (".error", "red"),
+    ]);
 
     let mut line = String::new();
     while let Ok(n) = std::io::stdin().read_line(&mut line) {
@@ -54,7 +68,7 @@ fn main() -> Result<(), NoneError> {
         }
         if let Ok(fields) = crate::fuchsia::parse_line(&line) {
             for field in fields {
-                print!("{}", field.format(&style)?);
+                print!("{}", field.format(&style_sheet)?);
             }
             print!("\n");
         }
