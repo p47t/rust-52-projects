cff83617157b6ca7fea04043c8bb1c68864ee80e
Patrick Tsai <patrick@simplypatrick.com>
2019-09-04 00:30:49 -0700

    [web-server] Initial commit

---
+++ web-server/src/main.rs
@@ -0,0 +1,44 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::fs::File;
+use std::time::Duration;
+use std::thread;
+use web_server::ThreadPool;
+
+fn main() {
+    let pool = ThreadPool::new(4);
+
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let mut file = File::open(format!("web-server/{}", filename)).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
